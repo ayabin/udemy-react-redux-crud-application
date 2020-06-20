0512adbcec975a6c549fb5bcd23e383f78c80285
ayabin <ayabin.jp@gmail.com>
2020-06-20 20:31:37 +0900

    learn props pf component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return <div>test</div>;
-  }
-}
-// function App() {
-//   // const greeting = 'Hi,Tom!';
-//   // const dom = <h1 className="foo">{greeting}</h1>;
-//   // return dom;
-//   //return <input type="text" onClick={() => console.log('i am clicked!')} />;
-//   //return <input type="text" onChange={() => console.log('i am changed!')} />;
-//   return (
-//     <div>
-//       <label htmlFor="bar">bar</label>
-//       <input type="text" onChange={() => console.log('i am changed!')} />
-//     </div>
-//   );
-// }
+const App = () => {
+  const profiles = [
+    { name: 'Taro', age: 15 },
+    { name: 'Hanako', age: 5 },
+    { name: 'NoName' },
+  ];
+  return (
+    <div>
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
+    </div>
+  );
+};
+
+const User = (props) => {
+  return (
+    <div>
+      Hi, I am {props.name}, and {props.age} years old.
+    </div>
+  );
+};
+
+User.defaultProps = {
+  age: 1,
+};
 
 export default App;
