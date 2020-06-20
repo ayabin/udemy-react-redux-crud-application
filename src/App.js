47d047da54c2ab38e94a7388006d572df3826fd0
ayabin <ayabin.jp@gmail.com>
2020-06-20 19:38:31 +0900

    learn jfx syntax

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 function App() {
+  // const greeting = 'Hi,Tom!';
+  // const dom = <h1 className="foo">{greeting}</h1>;
+  // return dom;
+  //return <input type="text" onClick={() => console.log('i am clicked!')} />;
+  //return <input type="text" onChange={() => console.log('i am changed!')} />;
   return (
     <div>
-      <h1>Hello World</h1>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => console.log('i am changed!')} />
     </div>
   );
 }
