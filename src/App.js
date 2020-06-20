1434be077de49c21353f3da32a265c86e58fea11
ayabin <ayabin.jp@gmail.com>
2020-06-20 20:02:52 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  // const greeting = 'Hi,Tom!';
-  // const dom = <h1 className="foo">{greeting}</h1>;
-  // return dom;
-  //return <input type="text" onClick={() => console.log('i am clicked!')} />;
-  //return <input type="text" onChange={() => console.log('i am changed!')} />;
-  return (
-    <div>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => console.log('i am changed!')} />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <div>test</div>;
+  }
 }
+// function App() {
+//   // const greeting = 'Hi,Tom!';
+//   // const dom = <h1 className="foo">{greeting}</h1>;
+//   // return dom;
+//   //return <input type="text" onClick={() => console.log('i am clicked!')} />;
+//   //return <input type="text" onChange={() => console.log('i am changed!')} />;
+//   return (
+//     <div>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => console.log('i am changed!')} />
+//     </div>
+//   );
+// }
 
 export default App;
